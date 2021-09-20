45207090a1b6cf598a9f9c5a59c3a4876d8b25f4
Pooja Rao <raopooj@gmail.com>
2021-09-20 14:05:09 +1000

    Creating navbar,header,footer and routes for home,login and signup

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-require('dotenv').config();
+//require('dotenv').config();
 
 const express = require('express');
-const morgan = require('morgan')
-const session = require('express-session')
+const morgan = require('morgan');
+//const session = require('express-session')
 
 const app = express();
 const PORT = process.env.PORT || 3000
+
+// routers
+//const loginRouter = require('./routes/login');
+//const signupRouter = require('./routes/signup');
+const homeRouter = require('./routes/homepage');
+//const logoutRouter = require('./routes/logout');
+
 
 // BODY PARSER
 app.use(express.urlencoded({ extended: false }));
@@ -18,20 +25,26 @@ app.set('view engine', 'ejs');
 
 //Set our static folder(CSS)
 app.use(express.static('public'));
-
-
 // Session config
-app.use(
-  session({
-    cookie: {
-      maxAge: 1000 * 60 * 60 * 24,
-    },
-    name: 'movie',
-    resave: false,
-    saveUninitialized: false,
-    secret: process.env.SESS_SECRET,
-  })
-);
+// app.use(
+//   session({
+//     cookie: {
+//       maxAge: 1000 * 60 * 60 * 24,
+//     },
+//     name: 'movies',
+//     resave: false,
+//     saveUninitialized: false,
+//     secret: process.env.SESS_SECRET,
+//   })
+// );
+
+//displaying pages using router
+app.use('/', homeRouter);
+//app.use('/signup', signupRouter);
+//app.use('/login', loginRouter);
+//app.use('/logout', logoutRouter);
+
+
 
 
 app.listen(PORT, () => {
