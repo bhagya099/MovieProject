fd82761f427c2567d697e84cf7db64f010850f5d
Bhagyashree <bhagya099@gmail.com>
2021-09-20 17:26:42 +1000

    changed the databse name in databse file

--- routes/signup.js
+++ routes/signup.js
@@ -20,11 +20,13 @@ router.post('/', (req, res) => {
   const { name, email, password, confirm_password } = req.body;
   console.log(req.body);
   // first check the user if exists
-  db.oneOrNone('SELECT * FOMR user WHERE email = $1;', email)
+  db.oneOrNone('SELECT * FROM user WHERE email = $1;', email)
     .then((userExists) => {
       if (userExists) {
         res.redirect('/signup?message=Email%20already%20exists.');
-      } else {
+      }
+      // if new users then create
+      else {
         //   convert the password to hash
         const salt = bcrypt.genSaltSync(10);
         const hash = bcrypt.hashSync(password, salt);
@@ -32,8 +34,11 @@ router.post('/', (req, res) => {
         //   changing the email intop lowecase and remove all the whitespace
         const cleanEmail = email.toLowerCase().trim();
         if (!name || !email || !password || !confirm_password) {
-          return res.redirect('/signup?message=Please%20fill%20the%name');
+          return res.redirect(
+            '/signup?message=Please%20fill%20the%all details'
+          );
         } else {
+          // check the password and confirm_password value should be same
           if (password !== confirm_password) {
             return res.redirect(
               '/signup?message=Passwrod%20should%20be%20match.'
