64f5de5be755d83e75e14e90f12b8ba0a217b68f
OlaShabalina <o.shabalina@outlook.com>
2021-09-20 21:17:34 +1000

    completed login page route set-up

--- routes/login.js
+++ routes/login.js
@@ -7,4 +7,42 @@ router.get('/', (req, res) => {
     res.render('pages/login');
 });
 
+router.post('/', (req, res) => {
+    const { email, password } = req.body;
+
+    // Backend validation for the login form
+    if ( !email || !password ) {
+        req.flash("error", "Please enter all fields");
+        res.redirect('/login');
+    } else {
+        // If the validation has passed:
+
+        // checking if the email exists in the database
+        db.oneOrNone('SELECT * FROM users WHERE email = $1;', email)
+        .then(user => {
+            if (!user) {
+                req.flash("error", "Either this email doesn't exists or password is not correct");
+                res.redirect('/login');
+            } else {
+
+                // if user exists, verify his password
+                bcrypt.compare(password, user.password)
+                .then(result => {
+                    if (result) {
+                        // if password is correct -> we create a user ID inside the session
+                        req.session.userId = user.users_id;
+                        res.redirect('/');
+                    } else {
+                        req.flash("error", "Either this email doesn't exists or password is not correct");
+                        res.redirect('/login')
+                    }
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+            };
+        });
+    };
+});
+
 module.exports = router;
