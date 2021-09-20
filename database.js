962021f3463298ac433de49636f3ce30162407fb
Pooja Rao <raopooj@gmail.com>
2021-09-20 11:44:08 +1000

    Create skeleton for project

---
+++ database.js
@@ -0,0 +1,21 @@
+  
+  // Proper way to initialize and share the Database object
+
+  // Loading and initializing the library:
+  const pgp = require('pg-promise')();
+
+  // Preparing the connection details:
+
+
+
+  // const cn = 'postgres://postgres:password@localhost:5432/my_schedules';
+  const cn = `postgres://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:5432/movie-db`;
+
+
+
+  // Creating a new database instance from the connection details:
+  const db = pgp(cn);
+
+
+  // Exporting the database object for shared use:
+  module.exports = db;
---
+++ sql/create_table.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS movies;
+
+CREATE TABLE IF NOT EXISTS users (
+    users_id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL, 
+    email VARCHAR(255) NOT NULL, 
+    password VARCHAR(255) NOT NULL);
+
+CREATE TABLE IF NOT EXISTS movies(
+    movie_id SERIAL PRIMARY KEY,
+    users_id INT NOT NULL,
+    rating INT CHECK (rating BETWEEN 1 AND 5) NOT NULL,
+    CONSTRAINT fk_users 
+    FOREIGN KEY(users_id)
+     REFERENCES users(users_id)
+     ON DELETE CASCADE
+);
