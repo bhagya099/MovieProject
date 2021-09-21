9b3428e359b70ba18c3aedd5efc6f3ae66582134
Bhagyashree <bhagya099@gmail.com>
2021-09-21 12:02:49 +1000

    delete the databse colsole log

--- database.js
+++ database.js
@@ -5,13 +5,10 @@
 
   // Preparing the connection details:
 
-  // const cn = 'postgres://postgres:password@localhost:5432/my_schedules';
-
   const cn = `postgres://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/movie_db`;
 
   // Creating a new database instance from the connection details:
   const db = pgp(cn);
 
-console.log(db);
   // Exporting the database object for shared use:
   module.exports = db;
