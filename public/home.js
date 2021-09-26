3cebb4a0852fd1c4f6da44b716015606e75210c7
Pooja Rao <raopooj@gmail.com>
2021-09-26 13:37:48 +1000

    Adding conditions, else statement not working

--- public/home.js
+++ public/home.js
@@ -6,23 +6,58 @@ const poster_URL = 'https://image.tmdb.org/t/p/original/';
 $.ajax(`${base_URL}/genre/movie/list${api_key}`)
 .then(data => {
    // console.log(data)
-    $("#genreSelection").append(`<option class="genre" value="0">Please select a genre</option>`)
-    $(data.genres).each((index) => {
-        $("#genreSelection").append(`<option class="genre" value="${data.genres[index].id}">${data.genres[index].name}</option>`);
-    });
-     // console.log($("#genreSelection").val());
-    console.log($("#searchText").val());
-    if($("#genreSelection").val() === 0){
-    //Display movies based on the genre selection
+  $("#genreSelection").append(`<option class="genre" value="0">Please select a genre</option>`)
+  $(data.genres).each((index) => {
+   $("#genreSelection").append(`<option class="genre" value="${data.genres[index].id}">${data.genres[index].name}</option>`);
+});
+// Events for different inputs (we will load different movie lists based on the filters)
+ $('#searchText, #genreSelection').on("input", (e) => {
+        e.preventDefault();
+        const isTextFound = $('#searchText').val() !== '';
+        const isGenreDefined = $("#genreSelection").val() !== '0';
 
-    $("#genreSelection").on("change", function (e) {
-    e.preventDefault();
-    console.log(e.target.value)
-    $(".film-list").empty()
-    $.ajax(`${base_URL}/discover/movie${api_key}&with_genres=${e.target.value}`) 
-    .then(data => {
-        //console.log(data)
-        for (let film of data.results) {
+        console.log(isTextFound)
+        console.log(isGenreDefined)
+
+        if (isTextFound && isGenreDefined) {
+             console.log("use two filters for the movies")
+        } else if (isTextFound && !isGenreDefined) {
+          console.log("use text filter for the movies")
+        
+          //Display movies based on the search text box
+          $('#searchText').on("input", function (e) {
+          e.preventDefault();
+            const searchTextValue = $(this).val();
+            console.log(searchTextValue);
+            $(".film-list").empty()
+            $.ajax(`${base_URL}/search/movie/${api_key}&query=${searchTextValue}`)
+            .then(data => {
+            //console.log(data)
+            for (let film of data.results) {
+            // console.log(film)
+            let newFilm = $("<tr>")
+            newFilm.append(`
+                <th scope="col"> <img src='${poster_URL}${film.poster_path}' style="width: 50px">  </th>
+                <td>${film.title}</td>
+                <td><i class="material-icons">star</i> <strong>9.2</strong></td>
+                <td><i class="material-icons">star_outline</i></td>
+                <td><a href="${film.id}"><i class="material-icons">info</i></a></td>`);
+                $(".film-list").append(newFilm)
+            }
+          });
+        });
+    } else if (!isTextFound && isGenreDefined) {
+        console.log("use genre filter for the movies")
+        //Display movies based on the genre selection
+
+        $("#genreSelection").on("change", function (e) {
+            e.preventDefault();
+            console.log(e.target.value)
+            $(".film-list").empty()
+            $.ajax(`${base_URL}/discover/movie${api_key}&with_genres=${e.target.value}`) 
+            .then(data => {
+            //console.log(data)
+            for (let film of data.results) {
            // console.log(film)
             let newFilm = $("<tr>")
             newFilm.append(`
@@ -35,32 +70,11 @@ $.ajax(`${base_URL}/genre/movie/list${api_key}`)
         }
       });
     });
-    }else if($("#genreSelection").val() == ''){
-
-    //Display movies based on the search text box
-
-    $('#searchText').on("input", function (e) {
-    e.preventDefault();
-    const searchTextValue = $(this).val();
-    console.log(searchTextValue);
-    $(".film-list").empty()
-    $.ajax(`${base_URL}/search/movie/${api_key}&query=${searchTextValue}`)
-    .then(data => {
-        //console.log(data)
-        for (let film of data.results) {
-           // console.log(film)
-            let newFilm = $("<tr>")
-            newFilm.append(`
-                <th scope="col"> <img src='${poster_URL}${film.poster_path}' style="width: 50px">  </th>
-                <td>${film.title}</td>
-                <td><i class="material-icons">star</i> <strong>9.2</strong></td>
-                <td><i class="material-icons">star_outline</i></td>
-                <td><a href="${film.id}"><i class="material-icons">info</i></a></td>`);
-            $(".film-list").append(newFilm)
-        }
-    });
-});
-}else{
+}
+    // } else {
+    //             console.log("display all popular movies")
+    //         }
+        })
         //Display all the popular movies
         $.ajax(`${base_URL}/movie/popular${api_key}`)
         .then(data => {
@@ -78,7 +92,7 @@ $.ajax(`${base_URL}/genre/movie/list${api_key}`)
         }
     });
     }
-});
+);
   
 
 
