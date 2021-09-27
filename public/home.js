5f21d6bb3a826091fc77861404ab9bd8b959224f
Pooja Rao <raopooj@gmail.com>
2021-09-27 10:53:25 +1000

    searching with keyword and genre not working

--- public/home.js
+++ public/home.js
@@ -5,7 +5,7 @@ const poster_URL = 'https://image.tmdb.org/t/p/original/';
 //Display all the genre from the api
 $.ajax(`${base_URL}/genre/movie/list${api_key}`)
 .then(data => {
-   // console.log(data)
+   console.log(data)
   $("#genreSelection").append(`<option class="genre" value="0">Please select a genre</option>`)
   $(data.genres).each((index) => {
    $("#genreSelection").append(`<option class="genre" value="${data.genres[index].id}">${data.genres[index].name}</option>`);
@@ -21,14 +21,40 @@ $.ajax(`${base_URL}/genre/movie/list${api_key}`)
 
         if (isTextFound && isGenreDefined) {
              console.log("use two filters for the movies")
+             $(".film-list").empty()
+             const searchTextValue = $('#searchText').val();
+             const genreValue = $("#genreSelection").val();
+             console.log(searchTextValue)
+             console.log(genreValue)
+              $.ajax(`${base_URL}/discover/movie/${api_key}&query=${searchTextValue}&${genreValue}`)
+              .then(data => {
+            //console.log(data)
+            for (let film of data.results) {
+            console.log(film.genre_ids)
+            let newFilm = $("<tr>")
+            film.genre_ids.forEach(i =>{
+                if(i ===genreValue){
+            newFilm.append(`
+                <th scope="col"> <img src='${poster_URL}${film.poster_path}' style="width: 50px">  </th>
+                <td>${film.title}</td>
+                <td><i class="material-icons">star</i> <strong>9.2</strong></td>
+                <td><i class="material-icons">star_outline</i></td>
+                <td><a href="${film.id}"><i class="material-icons">info</i></a></td>`);
+                $(".film-list").append(newFilm)
+            }
+            }) 
+        }
+        //  });
+        });
+              
         } else if (isTextFound && !isGenreDefined) {
           console.log("use text filter for the movies")
         
-          //Display movies based on the search text box
-          $('#searchText').on("input", function (e) {
-          e.preventDefault();
-            const searchTextValue = $(this).val();
-            console.log(searchTextValue);
+    //Display movies based on the search text box
+        //   $('#searchText').on("input", function (e) {
+         // e.preventDefault();
+            const searchTextValue = $('#searchText').val();
+            //console.log(searchTextValue);
             $(".film-list").empty()
             $.ajax(`${base_URL}/search/movie/${api_key}&query=${searchTextValue}`)
             .then(data => {
@@ -44,14 +70,14 @@ $.ajax(`${base_URL}/genre/movie/list${api_key}`)
                 <td><a href="${film.id}"><i class="material-icons">info</i></a></td>`);
                 $(".film-list").append(newFilm)
             }
-          });
+        //  });
         });
     } else if (!isTextFound && isGenreDefined) {
         console.log("use genre filter for the movies")
         //Display movies based on the genre selection
 
-        $("#genreSelection").on("change", function (e) {
-            e.preventDefault();
+        // $("#genreSelection").on("change", function (e) {
+        //     e.preventDefault();
             console.log(e.target.value)
             $(".film-list").empty()
             $.ajax(`${base_URL}/discover/movie${api_key}&with_genres=${e.target.value}`) 
@@ -67,18 +93,37 @@ $.ajax(`${base_URL}/genre/movie/list${api_key}`)
                 <td><i class="material-icons">star_outline</i></td>
                 <td><a href="${film.id}"><i class="material-icons">info</i></a></td>`);
             $(".film-list").append(newFilm)
-        }
+     }
       });
-    });
-}
-    // } else {
-    //             console.log("display all popular movies")
-    //         }
-        })
+  //  });
+} else {
+            console.log("display all popular movies")
+            //Display all the popular movies
+        $.ajax(`${base_URL}/movie/popular${api_key}`)
+        .then(data => {
+        //console.log(data)
+        $(".film-list").empty()
+        for (let film of data.results) {
+            //console.log(film)
+            let newFilm = $("<tr>")
+            newFilm.append(`
+                <th scope="col"> <img src='${poster_URL}${film.poster_path}' style="width: 50px">  </th>
+                <td>${film.title}</td>
+                <td><i class="material-icons">star</i> <strong>9.2</strong></td>
+                <td><i class="material-icons">star_outline</i></td>
+                <td><a href="${film.id}"><i class="material-icons">info</i></a></td>`);
+            $(".film-list").append(newFilm)
+             }
+          });
+    }
+  })
+}) 
+      //  })
         //Display all the popular movies
         $.ajax(`${base_URL}/movie/popular${api_key}`)
         .then(data => {
         //console.log(data)
+        $(".film-list").empty()
         for (let film of data.results) {
             //console.log(film)
             let newFilm = $("<tr>")
@@ -91,8 +136,6 @@ $.ajax(`${base_URL}/genre/movie/list${api_key}`)
             $(".film-list").append(newFilm)
         }
     });
-    }
-);
-  
+
 
 
