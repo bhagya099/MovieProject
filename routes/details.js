e818ac681a23dd2faeaa2b73718c2f2f8416b261
Bhagyashree <bhagya099@gmail.com>
2021-09-28 10:48:12 +1000

    only login user can submit the rating and also if the the user has already given rating then it can not do rating again

--- routes/details.js
+++ routes/details.js
@@ -6,10 +6,23 @@ const db = require("../database");
 router.get("/:id", (req, res) => {
   const userId = req.session.userId;
   const { id } = req.params;
-  res.render("./pages/details", {
-    id,
-    userId,
-    message: req.query.message,
+  db.oneOrNone(
+    "SELECT movie_id, users_id, rating FROM movies WHERE movies.users_id = $1 AND movie_id = $2;",
+    [req.session.userId, req.params.id]
+  ).then((rating) => {
+    console.log(rating);
+    if (rating) {
+      return res.render("./pages/details", {
+        message: `you already give rating ${rating.rating} to this movie  `,
+        id,
+        userId,
+      });
+    } else {
+      res.render("./pages/details", {
+        id,
+        userId,
+      });
+    }
   });
 });
 
@@ -19,34 +32,19 @@ router.post("/:id", (req, res) => {
   console.log(req.body.rating);
   console.log(req.session.userId);
   console.log(req.params.id);
-
-  db.oneOrNone(
-    "SELECT movie_id, users_id, rating FROM movies WHERE movies.users_id = $1 AND movie_id = $2;",
-    [req.session.userId, req.params.id]
+  db.none(
+    "INSERT INTO movies (movie_id, users_id, rating) VALUES ($1, $2, $3);",
+    [req.params.id, req.session.userId, req.body.rating]
   )
-    .then((rating) => {
-      console.log(rating);
-      if (rating) {
-      return res.send({
-        message: "you already give rating to this movie",
-      });
-      } else {
-        db.none(
-          "INSERT INTO movies (movie_id, users_id, rating) VALUES ($1, $2, $3);",
-          [req.params.id, req.session.userId, req.body.rating]
-        )
-          .then(() => {
-            console.log(rating);
-            console.log(req.session.userId);
-            res.redirect("/:id");
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-      }
+    .then(() => {
+      // console.log(rating);
+      console.log(req.session.userId);
+      res.redirect("/:id");
     })
     .catch((err) => {
       console.log(err);
+      res.send(err);
     });
 });
+  
 module.exports = router;
