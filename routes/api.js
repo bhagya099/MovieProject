8430009d83660d865cdc7bdcfe5a11203e001f71
OlaShabalina <o.shabalina@outlook.com>
2021-09-30 19:46:24 +1000

    cleaned up the api route, removed previous comments for passing user ratings only

--- routes/api.js
+++ routes/api.js
@@ -5,13 +5,8 @@ const db = require('../database');
 // This route is for passing the data from db to front-end
 
 router.get('/', (req,res) => {
-    // // getting value of the user id from the session
-    // const userId = req.session.userId;
 
-    // // Working on the part where we show loged in user rating next to each movie on the main page
-    // if (typeof userId !== 'undefined') {
-
-    // if user exists we are looking for his ratings
+    // getting all the ratings from the db
     db.any('SELECT movie_id, users_id, rating FROM movies;')
     .then((ratings) => {
 
@@ -25,12 +20,6 @@ router.get('/', (req,res) => {
         res.status(400).json({"error_message":"this page is not available"});
     });
 
-    // // if there is no user in the session, we don't need to pass anything to the page
-    // } else {
-    //     res.status(400).json({"error_message":"this page is not available"});
-    // }
-    
-    
 });
 
 module.exports = router;
